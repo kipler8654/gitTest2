058cfac2f0e2574cd554bf1582c9ee345fb2cabe
kipler8654 <adfd130@gmail.com>
2023-06-20 22:17:42 +0300

    test1

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+        Car sportCar = new Car.Builder("Sport wheels", "Sport body", "blue", "Sport interior").build();
+        Car classicCar = new Car.Builder("Classic wheels", "Classic body", "white", "Classic interior").build();
+        System.out.println(classicCar.equals(sportCar));
     }
 }
