5c55ce96b583a570d7b74b2b613e87678321ad8e
kipler8654 <adfd130@gmail.com>
2023-06-26 23:37:05 +0300

    test1

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,27 @@
+import java.io.*;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        try{
+            exception();
+        }catch (Exception e){
+            System.out.println("Exception error");
+        }
+
+    }
+    public static void exception() throws IOException {
+        try{
+            throw new NullPointerException();
+        }catch(NullPointerException e){
+            System.out.println("NullPointerException error");
+            throw new IOException();
+        }finally {
+            try (BufferedReader br = new BufferedReader(new FileReader("file.java"))) {
+                throw new ArrayIndexOutOfBoundsException();
+            } catch (IOException e) {
+                System.out.println("IOException error");
+            }
+        }
     }
 }
