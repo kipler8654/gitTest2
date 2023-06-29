7c0ec16463d342a65c7216ad7be219d1012b0889
kipler8654 <adfd130@gmail.com>
2023-06-29 23:09:46 +0300

    first commit

---
+++ src/CheckedException.java
@@ -0,0 +1,8 @@
+public class CheckedException extends Exception{
+    public CheckedException(){
+        super("Checked exception");
+    }
+    public CheckedException(String message){
+        super(message);
+    }
+}
