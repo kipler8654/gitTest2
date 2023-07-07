0e7819ec33828e2e97724b0dc007cb9ff3bb1e59
kipler8654 <adfd130@gmail.com>
2023-07-07 20:13:16 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,15 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
+        MyArraylist<String> arraylist = new MyArraylist<>();
+        arraylist.add("test");
+        arraylist.add("hi");
+        arraylist.add("hello");
+        arraylist.add("world");
+        arraylist.remove("world");
+        for (int i = 0; i < arraylist.getSize(); i++) {
+            System.out.print(arraylist.get(i) + " ");
+        }
     }
 }
---
+++ src/MyArraylist.java
@@ -0,0 +1,92 @@
+public class MyArraylist<T> {
+    private T[] array;
+    private int size;
+
+    public MyArraylist() {
+        array = (T[]) new Object[10];
+        size = 0;
+    }
+
+    public void add(T value) {
+        if (size >= array.length) {
+            T[] ar1 = (T[]) new Object[array.length * 2];
+            for (int i = 0; i < array.length; i++) {
+                ar1[i] = array[i];
+            }
+            array = ar1;
+        }
+        array[size] = value;
+        size++;
+    }
+
+    public void add(int index, T value) {
+        if (index < 0 || index >= size) {
+            System.out.println("Error");
+        }
+        if (size >= array.length) {
+            T[] ar1 = (T[]) new Object[array.length * 2];
+            for (int i = 0; i < array.length; i++) {
+                ar1[i] = array[i];
+            }
+            array = ar1;
+        }
+        for (int i = size; i > index; i--) {
+            array[size] = array[size - 1];
+        }
+        array[index] = value;
+        size++;
+    }
+
+    public void addAll(MyArraylist<T> list) {
+        for (int i = 0; i < list.size; i++) {
+            add(list.get(i));
+        }
+    }
+
+    public T get(int index) {
+        if (index < 0 || index >= size) {
+            System.out.println("Error");
+        }
+        return array[index];
+    }
+
+    public void set(T value, int index) {
+        array[index] = value;
+    }
+
+    public boolean isEmpty() {
+        if (size == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public T remove(int index) {
+        if (index < 0 || index >= size) {
+            System.out.println("Error");
+        }
+        T removedElement = array[index];
+        for (int i = index; i < size - 1; i++) {
+            array[i] = array[i + 1];
+        }
+        size--;
+        return removedElement;
+    }
+
+    public T remove(T value) {
+        for (int i = 0; i < getSize(); i++) {
+            if (array[i] == value) {
+                array[i] = array[i + 1];
+                size--;
+                return value;
+            }
+        }
+        System.out.println("NotFoundElement");
+        return null;
+    }
+}
